8c0b45ad5500d75e38b7963dddfe0e2d45bc6cf7
jinnxyoung <wlsdud7722@naver.com>
2021-07-27 07:54:22 +0900

    add CRUD

--- review/models.py
+++ review/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from imagekit.models import ImageSpecField
+#from imagekit.processors import ResizeToFill
 
 class Review(models.Model):
     post_title = models.CharField(max_length=100, default="제목없음")
@@ -7,6 +9,7 @@ class Review(models.Model):
     pub_date = models.DateTimeField()
     body = models.TextField(blank=True, null=True)
     image = models.ImageField(upload_to='review/', blank=True, null=True)
+    #image_thumbnail = ImageSpecField(source = 'image', processors = [ResizeToFill(250, 230)])
 
     def __str__(self):
         return self.post_title
