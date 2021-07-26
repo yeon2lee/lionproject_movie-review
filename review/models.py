b10aa27dd1f6f426a32a9754d255afa3044e5a0e
nayeon <md04254@naver.com>
2021-07-26 23:14:12 +0900

    add Review model

--- review/models.py
+++ review/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Review(models.Model):
+    post_title = models.CharField(max_length=100, default="제목없음")
+    movie_title = models.CharField(max_length=100, default="제목없음")
+    author = models.CharField(max_length=100, default="글쓴이없음")
+    pub_date = models.DateTimeField()
+    body = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='review/', blank=True, null=True)
+
+    def __str__(self):
+        return self.post_title
+
+    def summary(self):
+        return self.body[:30]
