96880f825480dc0a40692177140fc184f4b090b0
hildakim <pinkredjohn@gmail.com>
2021-07-27 14:19:59 +0900

    allauth setting
    settings.py, urls.py

--- movie_review/settings.py
+++ movie_review/settings.py
@@ -140,3 +140,12 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
